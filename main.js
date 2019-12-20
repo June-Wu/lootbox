6ba1e979a2561f50c5ec5cdcd877e135aab19551
June-Wu <june.wu.cs@gmail.com>
2019-12-20 14:22:36 -0800

    fixed precision problem

--- main.js
+++ main.js
@@ -1,102 +1,137 @@
+const bigDecimal = require('js-big-decimal');
+
+function buildAliasTable(itemsArray) {
+    // if(self.isEmpty(itemsArray)) {
+    //     console.log('empty table');
+    //     return [];
+    // }
+
+    const items = [];
+    let range = 0;
+    for(const i of itemsArray) {
+        if(!i.hasOwnProperty('weight') || Number.isNaN(Number(i.weight))) {
+            console.log("invalid item weight ", i);
+            return;
+        }
+        //cloning because table generation changes the items' weights
+        //item = self.clone(i);
+        const item = i;
+        range += item.weight;
+        item.weight = new bigDecimal(item.weight);
+        items.push(item);
+    }
+    const avg = new bigDecimal(range/items.length);
+    console.log('range ',range);
+
+    const worklistSmall = [];
+    const worklistLarge = [];
+
+    for(const item of items) {
+        if (item.weight.compareTo(avg) < 1) {
+            worklistSmall.push(item);
+        } else {
+            worklistLarge.push(item);
+        }
+    }
+
+    const aliasTable = [];
+    for(let i = 0; i<items.length; i++) {
+        if(worklistSmall.length > 0 && worklistLarge.length > 0) {
+            const s = worklistSmall.pop();
+
+            if (s.weight.compareTo(avg) === 0) {
+                //if we need each item's original weight, we can set it here
+                aliasTable.push({p:1, s:s});
+                continue;
+            }
+            const l = worklistLarge.pop();
+
+            //need to find a way to deal with this line. Avg could be a dec number and arithmetic  on this can be imprecise
+            const shortage = avg.subtract(s.weight);
+            l.weight = l.weight.subtract(shortage);
+            const p = s.weight.divide(avg);
+            aliasTable.push({p:p.getValue(), s:s, l:l});
+
+            if(l.weight.compareTo(avg) < 1) {
+                worklistSmall.push(l);
+            } else {
+                worklistLarge.push(l);
+            }
+            continue;
+        }
+
+        if(worklistSmall.length > 0) {
+            const s = worklistSmall.pop();
+            aliasTable.push({p:1, s:s});
+            continue;
+        }
+
+        if(worklistLarge.length > 0) {
+            console.log('precision error building Alias table');
+            const l = worklistLarge.pop();
+            aliasTable.push({p:1, s:l});
+        }
+    }
+    return aliasTable;
+}
+
+function pickFromAliasTable(aliasTable, random) {
+    // if (self.isEmpty(aliasTable)) {
+    //     console.log('empty table');
+    //     return;
+    //}
+    const index = Math.floor(Math.random() * aliasTable.length);
+    const column = aliasTable[index];
+
+    if(column.p === 1) {
+        return column.s;
+    }
+
+    const pick = Math.random();
+    const r = Math.floor(pick*100);
+    random[r] = random[r] || 0;
+    random[r]++;
+
+    if(pick < column.p) {
+        return column.s;
+    }
+    return column.l;
+}
+
+//////////////////////////////////////////////////////////
 function generateRandomSet(size) {
     let set = [];
-    let sum = 0;
     for(let i=0; i<size; i++) {
-        let prob = Math.random();
-        set.push({id:i, p:prob});
-        sum += prob;
+        let weight = Math.ceil((Math.random()-1)*-100);
+        set.push({id:i, weight:weight});
     }
-    set = _scaleSetProbability(set, sum);
     return set;
 }
 
-function _scaleSetProbability(set, sum) {
-    return set.map(function(x) {
-        x.p = (x.p * set.length)/sum;
-        return x;
-    });
-}
-
-function generateAliasTable(set) {
-    let small = [];
-    let big = [];
-
-    for(const item of set) {
-        if (item.p <= 1) {
-            small.push(item);
-        } else {
-            big.push(item);
-        }
-    }
-
-    let table = [];
-    for (let i = 0; i < set.length; i++) {
-        if (small.length > 0 && big.length > 0) {
-            let s = small.pop();
-            let b = big.pop();
-            b.p = b.p + s.p - 1;
-            if(b.p <= 1) {
-                small.push(b);
-            } else {
-                big.push(b);
-            }
-            table[i] = {p:s.p, s:s, b:b};
-            continue;
-        }
-
-        if (small.length > 0) {
-            let s = small.pop();
-            table[i] = {p:1, s:s};
-            continue;
-        }
-
-        if (big.length > 0) {
-            let b = big.pop();
-            table[i] = {p:0, b:b};
-            continue;
-        }
-    }
-    return table;
-}
-
-function lookupAliasTable(table) {
-    const column = Math.floor(Math.random() * table.length);
-    if (table[column].p < Math.random()) {
-        return table[column].s;
-    }
-    return table[column].b;
-}
-
-function lookUpStandard(set) {
-    const pick = Math.random() * set.length;
-    let accumulation = 0;
-    for (let i = 0; i < set.length; i++) {
-        accumulation += set[i].p;
-        if (accumulation > pick) {
-            return set[i];
-        }
-    }
-    console.log('range error');
-}
-
 function main() {
-    for(let i = 10; i < 10000; i += 10) {
-        for(let j = 0; j< 10; j++) {
-            const randomSet = generateRandomSet(i);
+    for(let i = 100; i < 101; i += 5) {
+        console.log(i);
+        const set = generateRandomSet(i);
+        console.log(set);
+        const alias = buildAliasTable(set);
+        console.log(alias);
+        console.log("alias.length ",alias.length);
 
+        const picked = {};
+        const randomCount = {};
+        for(let j=0; j<1000000; j++) {
             const t0 = process.hrtime();
-            lookUpStandard(randomSet);
+            const item = pickFromAliasTable(alias, randomCount);
             const t1 = process.hrtime(t0);
-
-            const aliasTable = generateAliasTable(randomSet);
-
-            const t2 = process.hrtime();
-            lookupAliasTable(aliasTable);
-            const t3 = process.hrtime(t2);
-
-            console.log(i, t1, t3);
-
+            if (item == null) {
+                console.log('oh no');
+            }
+            picked[item.id] = picked[item.id] || 0;
+            picked[item.id]++;
         }
+
+        console.log("item dist ", picked);
+        console.log("randomDist ",randomCount);
     }
 }
 
